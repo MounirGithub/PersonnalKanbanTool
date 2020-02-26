a1db5b8353601d212ef688afa28dda705033f5c0
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-02-26 14:32:41 +0100

    ProjectIdException

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/ProjectService.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package io.mounirKanban.ppmtool.services;
 
 import io.mounirKanban.ppmtool.domain.Project;
+import io.mounirKanban.ppmtool.exceptions.ProjectIdException;
 import io.mounirKanban.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,11 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier((project.getProjectIdentifier().toUpperCase()));
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("PROJECT ID '"+ project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
     }
-
 }
