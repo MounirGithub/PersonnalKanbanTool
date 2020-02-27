a36189ee8e244774bd7c7548e2338c61f0295e3c
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-02-27 20:04:28 +0100

    basic component

--- ppmtool-react-client-side/src/App.js
+++ ppmtool-react-client-side/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <Dashboard />
       </div>
     );
