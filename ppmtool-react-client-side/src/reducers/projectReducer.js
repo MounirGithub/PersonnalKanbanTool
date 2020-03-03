660130f9dddef308bea87758dc62c365881492f6
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-03 19:18:41 +0100

    get projects props

--- ppmtool-react-client-side/src/reducers/projectReducer.js
+++ ppmtool-react-client-side/src/reducers/projectReducer.js
@@ -10,7 +10,7 @@ export default function(state = initialState, action) {
     case GET_PROJECTS:
       return {
         ...state,
-        project: action.payload
+        projects: action.payload
       };
     default:
       return state;
