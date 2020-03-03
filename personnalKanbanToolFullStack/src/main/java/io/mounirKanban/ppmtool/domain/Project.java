27c11097b8372db77aff7203ad3308a0ff7d5973
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-03 21:38:35 +0100

    update a project-router-axios-reducer-type

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/domain/Project.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/domain/Project.java
@@ -26,6 +26,7 @@ public class Project {
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
     @JsonFormat(pattern = "yyyy-mm-dd")
+    @Column(updatable = false)
     private Date created_At;
     @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
