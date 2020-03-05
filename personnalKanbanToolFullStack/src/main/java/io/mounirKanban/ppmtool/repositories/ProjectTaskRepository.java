eabc92b76aae656874c5dff77fa8b13abf270d66
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-05 18:13:22 +0100

    find task by project sequence

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/repositories/ProjectTaskRepository.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/repositories/ProjectTaskRepository.java
@@ -8,5 +8,8 @@ import java.util.List;
 
 @Repository
 public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+
     List<ProjectTask> findByProjectIdentifierOrderByPriority(String id);
+
+    ProjectTask findByProjectSequence(String sequence);
 }
