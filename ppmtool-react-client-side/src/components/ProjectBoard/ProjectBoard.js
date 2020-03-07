8ebc8f9311d1343696ccdb47f6d3769f037337f0
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-07 23:49:00 +0100

    get tasks

--- ppmtool-react-client-side/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client-side/src/components/ProjectBoard/ProjectBoard.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Backlog from "./Backlog";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getBacklog } from "../../actions/backlogActions";
 
 class ProjectBoard extends Component {
+  //constructor to handle errors
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBacklog(id);
+  }
+
   render() {
     const { id } = this.props.match.params;
     return (
@@ -17,5 +27,13 @@ class ProjectBoard extends Component {
     );
   }
 }
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired
+};
 
-export default ProjectBoard;
+const mapStateToProps = state => ({
+  backlog: state.backlog
+});
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
