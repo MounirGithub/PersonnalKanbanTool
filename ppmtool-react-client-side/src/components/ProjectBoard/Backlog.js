72c08793046d2e994eb0228b3c088ffa81e6e5cf
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-08 17:56:55 +0100

    display projectTasks filterd status and priority

--- ppmtool-react-client-side/src/components/ProjectBoard/Backlog.js
+++ ppmtool-react-client-side/src/components/ProjectBoard/Backlog.js
@@ -3,6 +3,15 @@ import ProjectTask from "./ProjectTask/ProjectTask";
 
 class Backlog extends Component {
   render() {
+    const { project_tasks_prop } = this.props;
+
+    const tasks = project_tasks_prop.map(project_task => (
+      <ProjectTask key={project_task.id} project_task={project_task} />
+    ));
+
+    const filtredByStatus = (tasks, status) =>
+      tasks.filter(task => task.props.project_task.status === status);
+
     return (
       <div className="container">
         <div className="row">
@@ -12,7 +21,7 @@ class Backlog extends Component {
                 <h3>TO DO</h3>
               </div>
             </div>
-            <ProjectTask />
+            {filtredByStatus(tasks, "TO_DO")}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -20,6 +29,7 @@ class Backlog extends Component {
                 <h3>In Progress</h3>
               </div>
             </div>
+            {filtredByStatus(tasks, "IN_PROGRESS")}
           </div>
           <div className="col-md-4">
             <div className="card text-center mb-2">
@@ -27,6 +37,7 @@ class Backlog extends Component {
                 <h3>Done</h3>
               </div>
             </div>
+            {filtredByStatus(tasks, "DONE")}
           </div>
         </div>
       </div>
--- ppmtool-react-client-side/src/components/ProjectBoard/ProjectTask/ProjectTask.js
+++ ppmtool-react-client-side/src/components/ProjectBoard/ProjectTask/ProjectTask.js
@@ -2,15 +2,35 @@ import React, { Component } from "react";
 
 class ProjectTask extends Component {
   render() {
+    const { project_task } = this.props;
+
+    let priorityString;
+    let priorityClass;
+
+    if (project_task.priority === 1) {
+      priorityClass = "bg-danger text-light";
+      priorityString = "HIGH";
+    }
+
+    if (project_task.priority === 2) {
+      priorityClass = "bg-warning text-light";
+      priorityString = "MEDIUM";
+    }
+
+    if (project_task.priority === 3) {
+      priorityClass = "bg-info text-light";
+      priorityString = "LOW";
+    }
+
     return (
       <div className="card mb-1 bg-light">
-        <div className="card-header text-primary">
-          ID: projectSequence -- Priority: priorityString
+        <div className={`card-header text-primary ${priorityClass}`}>
+          ID: {project_task.projectSequence} -- Priority: {priorityString}
         </div>
         <div className="card-body bg-light">
-          <h5 className="card-title">project_task.summary</h5>
+          <h5 className="card-title">{project_task.summary}</h5>
           <p className="card-text text-truncate ">
-            project_task.acceptanceCriteria
+            {project_task.acceptanceCriteria}
           </p>
           <a href="" className="btn btn-primary">
             View / Update
