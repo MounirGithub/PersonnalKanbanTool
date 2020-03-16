f826e430223c17efc016458b95d7c5fde9a87109
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-16 17:47:13 +0100

    user password validation

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
@@ -24,6 +24,7 @@ public class UserService {
             newUser.setUsername(newUser.getUsername());
             // Make sure that password and confirmPassword match
             // We don't persist or show the confirmPassword
+            newUser.setConfirmPassword("");
             return userRepository.save(newUser);
 
         }catch (Exception e){
--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/web/UserController.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/web/UserController.java
@@ -3,6 +3,7 @@ package io.mounirKanban.ppmtool.web;
 import io.mounirKanban.ppmtool.domain.User;
 import io.mounirKanban.ppmtool.services.MapValidationErrorService;
 import io.mounirKanban.ppmtool.services.UserService;
+import io.mounirKanban.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,9 +25,14 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private UserValidator userValidator;
+
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
         // Validate passwords match
+        userValidator.validate(user,result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationErrorService(result);
         if(errorMap != null)return errorMap;
