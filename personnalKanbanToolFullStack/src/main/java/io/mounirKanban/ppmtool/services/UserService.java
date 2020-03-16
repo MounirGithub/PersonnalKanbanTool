282c7aaabd2a3b6f1c22bd0a9d40a7dc640fbcfa
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-16 17:36:38 +0100

    handle username already exists

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
@@ -1,6 +1,7 @@
 package io.mounirKanban.ppmtool.services;
 
 import io.mounirKanban.ppmtool.domain.User;
+import io.mounirKanban.ppmtool.exceptions.UsernameAlreadyExistsException;
 import io.mounirKanban.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,13 +17,18 @@ public class UserService {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public User saveUser (User newUser){
-        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
 
-        //Username has to be unique (exception)
+        try{
+            newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+            //Username has to be unique (exception)
+            newUser.setUsername(newUser.getUsername());
+            // Make sure that password and confirmPassword match
+            // We don't persist or show the confirmPassword
+            return userRepository.save(newUser);
 
-        // Make sure that password and confirmPassword match
-        // We don't persist or show the confirmPassword
-        return userRepository.save(newUser);
+        }catch (Exception e){
+            throw new UsernameAlreadyExistsException("Username '"+newUser.getUsername()+"' already exists");
+        }
     }
 
 }
