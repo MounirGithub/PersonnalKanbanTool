2dd5ac62eaa871ffbcf7dd0aea5e1fcc0e91e0c3
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-16 17:18:51 +0100

    create user happy path

--- personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
+++ personnalKanbanToolFullStack/src/main/java/io/mounirKanban/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package io.mounirKanban.ppmtool.services;
 
+import io.mounirKanban.ppmtool.domain.User;
 import io.mounirKanban.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +12,17 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    public User saveUser (User newUser){
+        newUser.setPassword(bCryptPasswordEncoder.encode(newUser.getPassword()));
+
+        //Username has to be unique (exception)
+
+        // Make sure that password and confirmPassword match
+        // We don't persist or show the confirmPassword
+        return userRepository.save(newUser);
+    }
 
 }
