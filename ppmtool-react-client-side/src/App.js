54516c50c7f70a23b36c8ddb50a600cf8eb2fc85
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-19 15:46:07 +0100

    prepare login register Header Landing templates with routing react

--- ppmtool-react-client-side/src/App.js
+++ ppmtool-react-client-side/src/App.js
@@ -11,6 +11,9 @@ import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTask/AddProjectTask";
 import UpdateProjectTask from "./components/ProjectBoard/ProjectTask/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/userManagement/Register";
+import Login from "./components/userManagement/Login";
 
 class App extends Component {
   render() {
@@ -19,6 +22,16 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
+
+            {
+              //public routes
+            }
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            {
+              //private routes
+            }
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/addProject" component={AddProject} />
             <Route exact path="/updateProject/:id" component={UpdateProject} />
