b9babd942c74fc5c0a14efc4beaf26d018179b44
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-03-23 15:40:40 +0100

    secureRoutes

--- ppmtool-react-client-side/src/App.js
+++ ppmtool-react-client-side/src/App.js
@@ -3,21 +3,22 @@ import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
-import AddProjectTask from "./components/ProjectBoard/ProjectTask/AddProjectTask";
-import UpdateProjectTask from "./components/ProjectBoard/ProjectTask/UpdateProjectTask";
 import Landing from "./components/Layout/Landing";
-import Register from "./components/userManagement/Register";
-import Login from "./components/userManagement/Login";
 import jwt_decode from "jwt-decode";
 import setJWTToken from "./securityUtils/setJWTToken";
 import { SET_CURRENT_USER } from "./actions/types";
 import { logout } from "./actions/securityAction";
+import SecuredRoute from "./securityUtils/SecureRoute";
+import Login from "./components/userManagement/Login";
+import Register from "./components/userManagement/Register";
+
+import { addProjectTask, updateProjectTask } from "./actions/backlogActions";
 
 const jwtToken = localStorage.jwtToken;
 
@@ -43,30 +44,41 @@ class App extends Component {
         <Router>
           <div className="App">
             <Header />
-
             {
-              //public routes
+              //Public Routes
             }
+
             <Route exact path="/" component={Landing} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
+
             {
-              //private routes
+              //Private Routes
             }
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/addProject" component={AddProject} />
-            <Route exact path="/updateProject/:id" component={UpdateProject} />
-            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-            <Route
-              exact
-              path="/addProjectTask/:id"
-              component={AddProjectTask}
-            />
-            <Route
-              exact
-              path="/updateProjectTask/:backlog_id/:pt_id"
-              component={UpdateProjectTask}
-            />
+            <Switch>
+              <SecuredRoute exact path="/dashboard" component={Dashboard} />
+              <SecuredRoute exact path="/addProject" component={AddProject} />
+              <SecuredRoute
+                exact
+                path="/updateProject/:id"
+                component={UpdateProject}
+              />
+              <SecuredRoute
+                exact
+                path="/projectBoard/:id"
+                component={ProjectBoard}
+              />
+              <SecuredRoute
+                exact
+                path="/addProjectTask/:id"
+                component={addProjectTask}
+              />
+              <SecuredRoute
+                exact
+                path="/updateProjectTask/:backlog_id/:pt_id"
+                component={updateProjectTask}
+              />
+            </Switch>
           </div>
         </Router>
       </Provider>
